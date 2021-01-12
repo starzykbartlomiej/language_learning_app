5adab1b9a688bd44c81a3073de3e52cb938d67a5
Bartek <starzyk.bartlomiej@gmail.com>
2021-01-12 14:57:17 +0100

    quizzes index page

--- Language-learning/project/app/Http/Controllers/QuizController.php
+++ Language-learning/project/app/Http/Controllers/QuizController.php
@@ -14,7 +14,8 @@ class QuizController extends Controller
      */
     public function index()
     {
-        //
+        $quizzes = Quiz::all();
+        return view('quizzes.index')->withQuizzes($quizzes);
     }
 
     /**
--- Language-learning/project/resources/views/layouts/navigation.blade.php
+++ Language-learning/project/resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('quizzes.index')" :active="request()->routeIs('quizzes.*')">
+                        {{ __('Quizzes') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
