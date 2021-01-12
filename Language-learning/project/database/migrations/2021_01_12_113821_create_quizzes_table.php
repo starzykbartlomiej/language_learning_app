5cab980b209488d66fd29f04111997c66fba09bd
Bartek <starzyk.bartlomiej@gmail.com>
2021-01-12 13:21:21 +0100

    added quiz-user 1:N relationship, working factory, migration

---
+++ Language-learning/project/database/migrations/2021_01_12_113821_create_quizzes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('language');
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+        });
+
+        Schema::dropIfExists('quizzes');
+    }
+}
--- Language-learning/project/routes/web.php
+++ Language-learning/project/routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Route::resource('/comments', App\Http\Controllers\CommentController::class);
 
+Route::resource('/quizzes', App\Http\Controllers\QuizController::class)->middleware('auth');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
