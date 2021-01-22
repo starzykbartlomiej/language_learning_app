1488ebec5b0626925045dade561888090c4d36eb
Bartek <starzyk.bartlomiej@gmail.com>
2021-01-22 14:17:52 +0100

    create_solutions_table

---
+++ Language-learning/project/database/migrations/2021_01_22_131112_create_solutions_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solutions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('question_id');
+            $table->string("answer");
+            $table->timestamps();
+
+            $table->foreign('question_id')
+                ->references('id')
+                ->on('questions')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('solutions', function (Blueprint $table) {
+            $table->dropForeign('question_id');
+            $table->dropForeign('user_id');
+        });
+        Schema::dropIfExists('solutions');
+    }
+}
