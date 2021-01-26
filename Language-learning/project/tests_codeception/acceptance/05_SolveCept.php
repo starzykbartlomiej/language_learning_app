77a4b80adce6c8ecd03c00e2a5b61866a85ef3d5
JakubDadela99 <jdadela@student.agh.edu.pl>
2021-01-26 17:56:05 +0100

    Solve test

--- Language-learning/project/tests_codeception/acceptance/05_SolveCept.php
+++ Language-learning/project/tests_codeception/acceptance/05_SolveCept.php
@@ -1,5 +1,26 @@
 <?php
 $I = new AcceptanceTester($scenario);
+$I->haveInDatabase('languages',[
+    'language'=>'spanish',
+]);
+$I->haveInDatabase('quizzes',[
+    'user_id'=>1,
+    'language_id'=>1,
+]);
+$I->haveInDatabase('questions',[
+    'quiz_id'=>1,
+    'answer'=>'hello',
+    'in_english'=>'Hola',
+    'is_correct'=>true,
+    'type'=>1
+]);
+$I->haveInDatabase('questions',[
+    'quiz_id'=>1,
+    'answer'=>'hello Jacob',
+    'in_english'=>'Hola Jacob',
+    'is_correct'=>true,
+    'type'=>2
+]);
 $I->wantTo('Create quizzes with specific languages');
 $I->amOnPage('/dashboard');
 $I->seeCurrentUrlEquals('/login');
@@ -11,4 +32,20 @@ $I->dontSee('Admin');
 $I->dontSee('Expert');
 $I->amOnPage('/quizzes');
 $I->seeCurrentUrlEquals('/quizzes');
+$I->see('Creator');
+$I->see('Language');
+$I->see('John Doe');
+$I->see('Solve');
+$I->click('Solve');
+$I->seeCurrentUrlEquals('/quizzes/1/solutions/create');
+$I->see('Translate word to Spanish');
+$I->see('Translate sentence to Spanish');
+$I->fillField('Translate sentence to Spanish','hello Jacob');
+$I->fillField('Translate word to Spanish','h');
+$I->click('Sumbit answers to the quiz');
+$I->seeInDatabase('solutions',[
+    'question_id'=>1
+]);
+$I->seeCurrentUrlEquals('/quizzes/1/solutions');
+$I->see('Quiz nr 1 results.');
 
